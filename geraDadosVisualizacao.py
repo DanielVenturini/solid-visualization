595ef8880b33b07bd807b77aeb0827074d114577
Daniel Venturini <danielventurini021@gmail.com>
2018-10-30 22:49:58 -0300

    Adicionando primeiro protótipo de visualização

--- geraDadosVisualizacao.py
+++ geraDadosVisualizacao.py
@@ -4,8 +4,28 @@ def geraDadosVisualizacao(file):
     dados = json.load(file)
     print("[")
     print("['Nome', 'Estrela', 'Forks', 'Grupo', 'Tamanho'],")
-    for repositorio in dados:
-        print("['{0}', '{1}', '{2}', '{3}', '{4}'],".format(repositorio['name'], repositorio['stargazers_count'], repositorio['forks_count'], 'Estrelas', repositorio['size']))
+    for i, repositorio in enumerate(dados):
+        print("['{0}', {1}, {2}, '{3}', {4}]".format(repositorio['name'], repositorio['stargazers_count'], repositorio['forks_count'], 'Estrelas', repositorio['size']))
+        if not i == len(dados)-1:       # não é o último repositório
+            print(",")                  # printa a vírgula
     print("]")
 
-geraDadosVisualizacao(open('Star1000.json'))
+def getLinguagens(file):
+	dados = json.load(file)
+	hashmap = {}
+
+	for repositorio in dados:
+		linguagem = repositorio['language']
+		try:
+			qtd = hashmap[str(linguagem)]
+			qtd += 1
+			hashmap[str(linguagem)] = qtd
+		except KeyError:
+			hashmap[str(linguagem)] = 1
+
+	for linguagem in hashmap.keys():
+		print('{value:' + str(hashmap.get(str(linguagem))) + ', name:"'+linguagem+'"},')
+
+getLinguagens(open('Star1000.json'))
+
+#geraDadosVisualizacao(open('Star1000.json'))
